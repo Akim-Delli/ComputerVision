f4f898dbe00cf3476c90b5f6ad7f770ad92d6107
akim <akim_delli@hotmail.com>
2016-06-13 22:41:03 -0400

    cropped, risize, rotate ...basic image operations

--- pil.py
+++ pil.py
@@ -1,3 +1,25 @@
 from PIL import Image
 
-pil_im = Image.open('empire.jpg')
+# convert an image to thumbnail
+pil_im = Image.open('data/empire.jpg')
+pil_im.thumbnail((128, 128))
+pil_im.save('data/output/empire-thumbnail.jpg')
+
+# Cropping a region of an image
+pil_img2 = Image.open('data/empire.jpg')
+box = (100, 100, 400, 400)
+region = pil_img2.crop(box)
+region = region.transpose(Image.ROTATE_180)
+pil_img2.paste(region, box)
+pil_img2.save('data/output/empire-cropped.jpg')
+
+# Resize
+pil_im3 = Image.open('data/empire.jpg')
+out = pil_im3.resize((128, 128))
+out.save('data/output/empire-resize.jpg')
+
+# Rotate
+out = pil_im3.rotate(45)
+out.save('data/output/empire-rotate.jpg')
+
+
